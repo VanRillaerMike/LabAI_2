c12657664acc0fae9c48ccb3a99a0697e18ba295
VanRillaerMike <mike.vanrillaer.work@gmail.com>
2021-11-25 14:22:55 +0100

    edited classification_options.py

--- options/classification_options.py
+++ options/classification_options.py
@@ -5,12 +5,12 @@ class ClassificationOptions(Options):
     def __init__(self):
         super().__init__()
         # dataset related
-        self.batch_size_test = 128
+        self.batch_size_test = 128                 #128
         self.batch_size_train = 128
 
         # hyperparameters
-        self.lr = 0.0001
-        self.num_epochs = 10
-        self.hidden_sizes = [784, 196, 49, 10]
+        self.lr = 0.0001                            #0.0001
+        self.num_epochs = 10                       #10
+        self.hidden_sizes = [784, 400, 400, 10]      #784, 196, 49, 10
 
 #        self.hidden_sizes = [784, 526, 268, 10]
